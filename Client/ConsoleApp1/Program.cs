4e0b2fd18a162ffa1b640ba78fa15d91db6b6c3e
RuLe0G <khlebnikov2015@yandex.ru>
2019-04-18 11:31:54 +0700

    First

---
+++ Client/ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //создание объекта для отслеживания сообщений переданных с ip адреса через порт
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            //начало прослушивания
+            listener.Start();
+            //цикл подключения клиентов
+            while (true)
+            {
+                //принятие запроса на подключение
+                TcpClient client = listener.AcceptTcpClient();
+                //создание нового потока для обслуживания нового клиента
+                Thread clientThread = new Thread(() => Process(client));
+                clientThread.Start();
+            }
+            if (listener != null)
+                listener.Stop();
+        }
+
+        const int port = 8888;
+        //объект, прослушивающий порт
+        static TcpListener listener;
+
+        //функция обработки сообщений от клиента
+        public static void Process(TcpClient tcpClient)
+        {
+            TcpClient client = tcpClient;
+            NetworkStream stream = null;
+            try
+            {
+                //получение потока для обмена сообщениями
+                stream = client.GetStream();
+                // буфер для получаемых данных
+                byte[] data = new byte[64];
+                //цикл обработки сообщений
+                while (true)
+                {
+                    //объект, для формирования строк
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    //до тех пор, пока в потоке есть данные
+                    do
+                    {
+                        //из потока считываются 64 байта и записываются в data
+                        bytes = stream.Read(data, 0, data.Length);
+                        //из считанных данных формируется строка
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    //преобразование сообщения
+                    string message = builder.ToString();
+                    //вывод сообщения в консоль сервера
+                    Console.WriteLine(message);
+                    //преобразование сообщения в набор байт
+                    data = Encoding.Unicode.GetBytes(message);
+                    //отправка сообщения обратно клиенту
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+3
+ catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //освобождение ресурсов при завершении сеанса
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+}
---
+++ Clientt/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Clientt
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int port = 8888;
+        const string address = "10.23.168.50";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Port.Text = port.ToString();
+            IP.Text = address;
+
+        }
+
+        string userName;
+        bool st = false;
+        NetworkStream stream;
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+
+            Thread myThread1 = new Thread(new ThreadStart(Count1));
+            myThread1.Start();
+
+
+        }
+
+        public void Count1()
+        {
+            string userName = name.Text.ToString();
+
+            TcpClient client = null;
+            try
+            {
+
+                client = new TcpClient(address, port);
+
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    st = true;
+                }
+            }
+
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                client.Close();
+            }
+
+        }
+
+        public void Count2()
+        {
+            
+            Content.Text += userName + ": ";
+            string message = Inp.Text.ToString();
+            Content.Text += message;
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+            data = new byte[64];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;            do
+            {
+                
+                bytes = stream.Read(data, 0, data.Length);
+                
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable);
+            message = builder.ToString();            Content.Text = ("Сервер: " + message);
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            Thread myThread2 = new Thread(new ThreadStart(Count2));
+            myThread2.Start();
+        }
+    }
+}
+
