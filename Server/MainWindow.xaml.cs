5a260814c43a7d656a9d6c5f397c40d5729d6edd
RuLe0G <khlebnikov2015@yandex.ru>
2019-04-24 12:55:29 +0700

    smt

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -29,9 +29,10 @@ namespace Server
 
         public MainWindow()
         {
-            InitializeComponent();            ContentDa.Text = "Начало";
+            InitializeComponent();
             listener = new TcpListener(IPAddress.Parse("10.23.168.50"), port);
             listener.Start();
+            ContentDa.Text += "сервер запущен\n";
             Thread serverThread = new Thread(() => Count1());
             serverThread.Start();
 
@@ -39,59 +40,92 @@ namespace Server
 
         public void Count1()
         {
-            while (true)
+            try
             {
-                
-                TcpClient client = listener.AcceptTcpClient();
-                
-                Thread clientThread = new Thread(() => Process(client));
-                clientThread.Start();
+                while (true)
+                {
 
-                //if (listener != null)
-                //{
-                //    listener.Stop();
-                //    break;
-                //}
-            }                               
-        }
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    Thread clientThread = new Thread(() => Process(client));
+                    clientThread.Start();
+
+                    //if (listener != null)
+                    //{
+                    //    listener.Stop();
+                    //    break;
+                    //}
+                }
+            }
+            catch(Exception ex)
+            {
+                ContentDa.Text += (ex.Message +  " Потеря соеденения\n");
+            }
+            }
+
+
 
         public static void Process(TcpClient tcpClient)
         {
             TcpClient client = tcpClient;
             NetworkStream stream = null;
             try
-            {                
+            {
                 stream = client.GetStream();
                 byte[] data = new byte[64];
-               
-                    StringBuilder builder = new StringBuilder();
-                    int bytes = 0;
-                    do
+                try
+                {
+                    while (true)
                     {
-                        bytes = stream.Read(data, 0, data.Length);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0;
+                        do
+                        {
+                            bytes = stream.Read(data, 0, data.Length);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        while (stream.DataAvailable);
+
+                        string message = builder.ToString();
+
+                        if (message == "ddiissccoonnneecctteedd")
+                        {
+                            stream.Close();
+                            client.Close();
+                            //ContentDa.Text += (ex.Message + " Потеря соеденения\n");
+                            MessageBox.Show(" Потеря соеденения");
+                            break;
+                        }
+                        //}
+                        //string message1 = "";
+                        //for (int i = message.Length - 1; i >= 0; i--)
+                        //{
+                        //    message1 += message[i].ToString();
+                        //}
+                        //data = Encoding.Unicode.GetBytes(message1);
+                        //stream.Write(data, 0, data.Length);
+
+                        data = Encoding.Unicode.GetBytes(message);
+                        stream.Write(data, 0, data.Length);
                     }
-                    while (stream.DataAvailable);
+                }
+                catch (Exception ex)
+                {
 
-                    string message = builder.ToString();
-                ContentDa.text += message;
-                
-                    data = Encoding.Unicode.GetBytes(message);
-                    stream.Write(data, 0, data.Length);
-             
+                }
+                finally
+                {
+                    if (stream != null)
+                        stream.Close();
+                    if (client != null)
+                        client.Close();
+                }
             }
-            catch (Exception ex)
-            {
-                ContentDa.text += ex;
             }
-            finally
-            {
-                if (stream != null)
-                    stream.Close();
-                if (client != null)
-                    client.Close();
-            }
+        public void Disconnected__Click(object sender, RoutedEventArgs e)
+        {
+            listener.Stop();
+            ContentDa.Text += "Потеря соеденения\n";
         }
-
     }
 }
